0329795c491666c10d3cc0b3663e95187d8477ec
ElielZuin <142356445+ElielZuin@users.noreply.github.com>
2025-04-29 20:19:56 -0300

    Add files via upload

---
+++ transformar celsius em F.lua
@@ -0,0 +1,6 @@
+local c, f
+io.write("Digite a temperatura em graus Celsius: ")
+c=tonumber(io.read())
+--Transformar--
+f = 9*c/5 + 32
+io.write("A temperatura em graus Fharenheité: ",f,"\n")
---
+++ while.lua
@@ -0,0 +1,10 @@
+local i=0
+while(i <= 10) do
+  if (<10) then
+    io.write(i,",")
+  else
+    io.write(i,"\n")
+  end
+i=i+1
+end
+io.write("\n")
