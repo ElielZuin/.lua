9ddd35f646cc088865e22bed7897586df51f6380
ElielZuin <142356445+ElielZuin@users.noreply.github.com>
2025-04-29 20:19:01 -0300

    Add files via upload

---
+++ read e write lua.lua
@@ -0,0 +1,7 @@
+local nota1, nota2, media
+io.write("Digite a primeira nota: ")
+nota1=tonumber(io.read())
+io.write("Digite a segunda nota: ")
+nota2=tonumber(io.read())
+media = (nota1 + nota2)/2
+io.write("A média aritmética é: ",media,"\n")
